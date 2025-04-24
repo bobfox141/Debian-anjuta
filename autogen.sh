b04713cd91a11717f8aa2756c8335968c6641ad0
Steven Garbeil <bobfox141@gmail.com>
2025-04-24 13:14:09 -0500

    DOS2UNIX game endings should be correct now

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,22 @@
-#!/bin/sh
+#!/usr/bin/bash
 # Run this to generate all the initial makefiles, etc.
-
+echo "[INFO] starting autogen setting source"
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-echo "Generating initial interface files"
-sh -c "cd $srcdir/libanjuta/interfaces && \
+echo "[INFO] Generating initial interface files"
+bash -c "cd $srcdir/libanjuta/interfaces && \
 perl anjuta-idl-compiler.pl libanjuta && \
 touch iface-built.stamp"
-
+echo "[INFO] testing srcdir implementation"
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 (
  cd "$srcdir" &&
  gtkdocize &&
- autopoint --force &&
+ echo "[INFO] gtkdocsized" &&
+ autopoint --force && 
+ echo "[INFO] autopoint" &&
  AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
 ) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
